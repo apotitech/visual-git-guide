2739facb4bd95ff70fd705494e07223ea2db0076
apotitech <apotitech@users.noreply.github.com>
2023-11-30 05:08:35 +0000

    deploy: 870aec48ffa265747b9fc00dee45f2214eec94cc

--- visual-git-guide.js
+++ visual-git-guide.js
@@ -49,3 +49,11 @@ _gaq.push(['_trackPageview']);
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+
+// Adding black background
+document.addEventListener('DOMContentLoaded', (event) => {
+  const toggleButton = document.getElementById('dark-mode-toggle');
+  toggleButton.addEventListener('click', function() {
+      document.body.classList.toggle('dark-mode');
+  });
+});
